58c03164f3de753f342a3401c1728260257e52d7
alex@H710 <babba@mail.de>
2016-12-31 00:14:10 +0100

    move todo from code to readme

--- AutoHome/FrmMain.cs
+++ AutoHome/FrmMain.cs
@@ -10,18 +10,7 @@ using cpsLIB;
 using System.Runtime.InteropServices;
 
 //System.IO.File.AppendAllText(@"\\ATOM\alex\desktop\autohomedbg.txt", "" + Environment.NewLine);
-#region TODO
-/* TODO: liste
- * im Kontext UDP -> reaktionszeitentests unter last -> requests werden von PLC schneller beantwortet als in GUI dargestellt (siehe wireshark trace)
- * plc_client -> mehrere frames in container packen und auf einmal versenden
- * file save -> in zip nur die relevanten files übernehmen
- * platform -> hinterlegte hintergrundbilder beim Import mit anbieten
- * log4net -> eigene logging funktion austauschen
- * mySQL -> statistic mit connector in eigenes Add On auslagern für Applikationen ohne Datenbankanbindung
- * sensorik generisch machen -> in liste analog zu aktor verwalten und in "interprete_frame_funkt()" anpassen
- * FrmConfigPlatform: einen art hardwarekatalog darstellen in dem jeder aktor einmal angezeigt wird
-*/
-#endregion
+
 
 namespace AutoHome
 {
